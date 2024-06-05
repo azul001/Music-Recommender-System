6b269983aa6dccf76abc54aa906b6d443eb96619
Prince Markose <85405580+azul001@users.noreply.github.com>
2024-06-05 20:34:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+import pickle
+import streamlit as st
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+CLIENT_ID = "cc530edf107e42dfaf714f0034eda539"
+CLIENT_SECRET = "5c2f0d2c93ed403fa3a33861284f03c9"
+
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def get_song_album_cover_url(song_name, artist_name):
+    search_query = f"track:{song_name} artist:{artist_name}"
+    results = sp.search(q=search_query, type="track")
+
+    if results and results["tracks"]["items"]:
+        track = results["tracks"]["items"][0]
+        album_cover_url = track["album"]["images"][0]["url"]
+        return album_cover_url
+    else:
+        return "https://i.postimg.cc/0QNxYz4V/social.png"
+
+def recommend(song):
+    index = music[music['song'] == song].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_music_names = []
+    recommended_music_posters = []
+    for i in distances[1:6]:
+        artist = music.iloc[i[0]].artist
+        recommended_music_posters.append(get_song_album_cover_url(music.iloc[i[0]].song, artist))
+        recommended_music_names.append(music.iloc[i[0]].song)
+
+    return recommended_music_names, recommended_music_posters
+
+st.header('Music Recommender System')
+music = pickle.load(open('df.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+music_list = music['song'].values
+selected_song = st.selectbox(
+    "Type or select a song from the dropdown",
+    music_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_music_names, recommended_music_posters = recommend(selected_song)
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.text(recommended_music_names[i])
+            st.image(recommended_music_posters[i])
